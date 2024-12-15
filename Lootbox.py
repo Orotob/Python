760ff0dc0b9786e88b9a13b69a2d76fd98b95017
Orotob <oromatob@gmail.com>
2024-12-14 18:33:44 -0600

    Update Lootbox.py

--- Lootbox.py
+++ Lootbox.py
@@ -1,1 +1,17 @@
+import random
+import math
 
+#Global Variables
+Rate = []
+Legendary = 100000
+Epic = 1000
+Rare = 100
+Uncommon = 10
+Common = print("You have obtained a Common item")
+Lootbox = 
+
+
+Rate = random.choice(x)
+
+def Lootbox(x):
+  
