01df5d51e33c8aba30253745f5b2ff482cffd06c
Orotob <oromatob@gmail.com>
2024-12-14 19:26:20 -0600

    Test

---
+++ Lootbox Simulator/Lootbox.py
@@ -0,0 +1,7 @@
+import random
+import math
+from LootboxSimulation import Rates
+#Global Variables
+print(len(100000))
+
+  
---
+++ Lootbox Simulator/LootboxSimulation.py
@@ -0,0 +1,17 @@
+import random
+import math
+import time
+
+class Rates():
+    def __init__(self):
+        self.Legendary = 100000
+        self.Epic = 100000
+        self.Rare = 1000
+        self.Uncommon = 100
+        self.Common = print("You have obtained a Common item")
+        self.rate = random.choice(x)
+        self.rarity = random.randint(0,110000)
+    def Roll(self):
+        print("Opening Lootbox...")
+        sleep(3)
+        
